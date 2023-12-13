8a962994a44e0b9e00949757443742363772811b
zdzd3643 <zdzd3643@gmail.com>
2023-12-13 18:37:23 +0900

    ff

--- 02.다트연습/01.변수.dart
+++ 02.다트연습/01.변수.dart
@@ -1,10 +1,25 @@
 void main(List<String> args) {
+  // 다트언어는 변수형을 자동으로 인식하는 선언이 있음
+  // 1. var
   var myHome = "내집이야~!";
   print(myHome);
   // var는 처음에 할당한 값의 형으로 재할당해야함!
   // myHome = 32;
   myHome = "너의집도 크다!";
   print(myHome);
+
+  // 2. dynamic
+  // -> var와 비슷하지만 나중에 재할당시
+  // 처음 할당한 값과 다른형의 값을 넣어도 괜찮다!
+  dynamic myId = "hhh2345";
+
+  print("나의 아이디는 $myId");
+
+  myId = 787878;
+
+  print("너의 아이디는 $myId");
+
+  // [ 문자형 출력방법 : '${코드}' 또는 '${변수}']
 
   // 다트언어는 변수의 형을 선언함이 기본이다!
   // 1. 숫자형 : 
@@ -39,7 +54,9 @@ void main(List<String> args) {
   print(we.length);
 
   // 4-2. Set형 - 순서가 없고 중복되지 않는 데이터집합
-  Set<int> evens = {2,4,6,8,10};
+  // Set<int> evens = {2,4,6,8,10};
+  // Set<dynamic> evens = {2,4,6,8,10,4,'짝수'};
+  var evens = {2,4,6,8,10, '짝수'};
   print(evens);
 
   // 4-3. Map형 - 데이터에 라벨링된 데이터 집합(객체와 유사)
